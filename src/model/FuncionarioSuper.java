b851374ce0bf242e5621862c9470c5dc4181a191
momed003 <yassiminm43@gmail.com>
2023-06-22 23:22:57 +0100

    FuncionarioSuper.java commit

---
+++ src/model/FuncionarioSuper.java
@@ -0,0 +1,60 @@
+package model;
+
+public class FuncionarioSuper {
+    private String nome;
+    private int idade;
+    private String cargo;
+
+    FuncionarioSuper() {
+
+    }
+
+    FuncionarioSuper(String nome, int idade, String cargo) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    private double salario;
+    private double bonus;
+}
