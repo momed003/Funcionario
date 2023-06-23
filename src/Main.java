480b53c601ae43f4bfbfef9d727a88c31624187e
momed003 <yassiminm43@gmail.com>
2023-06-23 13:44:58 +0100

    main.java. commit

--- src/Main.java
+++ src/Main.java
@@ -16,28 +16,29 @@ public class Main {
         int op=sc.nextInt();
         switch (op){
             case 1:
+
                 System.out.println("digite o nome:");
-                String nome=sc.nextLine();
+                gerente.setNome(sc.next());
                 System.out.println("digite sua idade: ");
-                int idade=sc.nextInt();
+                gerente.setIdade(sc.nextInt());
                 System.out.println("digite sua carga horaria: ");
-                int carga=sc.nextInt();
+                gerente.setCargaH(sc.nextInt());
                 System.out.println("digite seu ordenado mensal: ");
-                double salario=sc.nextDouble();
+                gerente.setSalario(sc.nextDouble());
 
-                gerente.setNome(nome);
-                gerente.setCargaH(carga);
-                gerente.setIdade(idade);
-                gerente.setSalario(salario);
-                gerente.bonus();
-                gerente.funcao();
-                gerente.assiduo();
-                gerente.desempenho();
+                    sc.close();
 
-                System.out.println(gerente.getNome());
-                System.out.println(gerente.getIdade());
-                System.out.println(gerente.bonus());
-                System.out.println(gerente.CargaHoraria());
+                System.out.println("------------------------------");
+                System.out.println("seu nome: "+gerente.getNome());
+                System.out.println("idade:"+gerente.getIdade());
+                System.out.println("bonus anual: "+gerente.bonus()+"mts");
+                System.out.println("carga horaria: "+gerente.CargaHoraria());
+                System.out.println("suas funcoes: "+ gerente.funcao());
+                System.out.println("assiduo: "+gerente.assiduo());
+                System.out.println("desempeho: "+gerente.desempenho());
+                System.out.println("-----------------------------");
+            case 2:
+
         }
 
 
